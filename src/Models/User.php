3cbad096eb03fec25d0a05d9f08a4fe4f3ae6c72
Nguyen Dinh Tuan <junnguyenvizito@gmail.com>
2023-11-25 12:02:35 +0700

    delete giao diện

--- src/Models/User.php
+++ src/Models/User.php
@@ -7,9 +7,15 @@ use Group4\BaseMvc\Model;
 class User extends Model {
     protected $table = 'user';
     protected $columns = [
-        'name',
-        'email',
+        'avatar',
+        'username',
+        'phone',
+        'birth_day',
         'address',
+        'email',
         'password',
+        'sex_id',
+        'role_id',
+        'status_id'
     ];
 }
