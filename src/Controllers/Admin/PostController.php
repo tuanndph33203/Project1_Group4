eede38fea545e6164f53aa2c585b23ea0562d6ae
Nguyen Dinh Tuan <junnguyenvizito@gmail.com>
2023-12-08 23:49:01 +0700

    sửa lỗi tăng giảm cart

--- src/Controllers/Admin/PostController.php
+++ src/Controllers/Admin/PostController.php
@@ -20,22 +20,41 @@ class PostController extends Controller
     }
     public function update()
     {
-        $posts = (new Post())->all('post_id');
+        $post = (new Post())->findOne('post_id',$_GET['id']);
         $this->renderAdmin(
             "post/update",
             [
-                "posts" => $posts
+                "post" => $post
             ]
         );
     }
+    public function delete()
+    {
+        $conditions = [
+            ['post_id', '=', $_GET['id']],
+        ];
+        (new Post())->delete($conditions);
+        header('Location:/admin/post');
+    }
     public function create()
     {
-        $posts = (new Post())->all('post_id');
-        $this->renderAdmin(
-            "post/create",
-            [
-                "posts" => $posts
-            ]
-        );
+        if (isset($_POST["add_post"])) { 
+            $image = $_FILES['image'];
+            $folder = "assets/img/cart/";
+            $file_name = basename($image['name']);
+            $target = $folder . $file_name;
+            move_uploaded_file($image["tmp_name"], $target);
+            $time = date("Y-m-d H:i:s");
+            $data = [
+                'title' => $_POST['title'],
+                'content' => $_POST['content'],
+                'image' => $file_name,
+                'created_at' => $time,
+                'updated_at' => null
+            ];
+            (new Post())->insert($data);
+            header('Location:/admin/post/create');
+        }
+        $this->renderAdmin("post/create");
     }
 }
